a68d79ee057b25a05fb7002147f7f4d219d3146f
Rytis Petronis <petronisrytis@gmail.com>
2021-03-08 22:38:30 +0200

    add testing layout

--- UI/PackageResources/PackageResourcesSample/PackageResourcesSample.Shared/MainPage.xaml.cs
+++ UI/PackageResources/PackageResourcesSample/PackageResourcesSample.Shared/MainPage.xaml.cs
@@ -16,7 +16,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-using FrameGenerator;
 using ICSharpCode.SharpZipLib.Zip;
 using SkiaSharp;
 using SkiaSharp.Views.UWP;
@@ -33,15 +32,12 @@ namespace PackageResourcesSample
         private CancellationTokenSource cancellations;
         private IList<SampleBase> samples;
         private SampleBase sample;
-        private MainGenerator generator;
         public MainPage()
         {
             InitializeComponent();
             samples = SamplesManager.GetSamples().ToList();
             SamplesInitializer.Init();
             var o = new ToggleSwitch { FlowDirection = FlowDirection.RightToLeft };
-
-            generator = new MainGenerator(new UnoFileReader(), 69);
             SetSample(samples.First());
         }
 
@@ -123,52 +119,16 @@ namespace PackageResourcesSample
             sample?.Tap();
         }
 
-        public async void LoadPackageFile(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var file = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(new Uri(assetFileName.Text));
-                var bytes = await FileIO.ReadBufferAsync(file);
-                var stream = bytes.AsStream();
-                await ExtractExtraFileFolder(stream);
-                output.Text = await FileIO.ReadTextAsync(file);
-
-            }
-            catch (Exception ex)
-            {
-                output.Text = ex.ToString();
-            }
-        }
-
-        public async Task LoadDCSSImage()
-        {
-
-                await generator.InitialiseGenerator();
-                var skBitmap = generator.GenerateImage(null);
-                output.Text = $"{skBitmap.Height}, {skBitmap.Width}";
-
-                var bitmapSource = new WriteableBitmap(1602, 768);
-
-                using (Stream stream = bitmapSource.PixelBuffer.AsStream())
-                {
-                    //write to bitmap
-                    await stream.WriteAsync(skBitmap.Bytes, 0, skBitmap.Bytes.Length).ConfigureAwait(false);
-                }
-                myImage.Source = bitmapSource;
-
-        }
-
-
 
         public async Task WriteAnotherImage()
         {
             var skBitmap = new SKBitmap(new SKImageInfo(1602, 768));
             using (var canvas = new SKCanvas(skBitmap))
             {
-                canvas.Clear(SKColors.Red);
+                canvas.Clear(SKColor.Parse(color.Text));
             }
-
-            output.Text = $"switch";
+            
+            output.Text = $"loaded, uint byte maxvalue = {(uint)byte.MaxValue}";
 
             var bitmapSource = new WriteableBitmap(1602, 768);
             using (Stream stream = bitmapSource.PixelBuffer.AsStream())
@@ -180,111 +140,85 @@ namespace PackageResourcesSample
 
         }
 
-
-        public async Task StartImageLoop()
+        private async void Button_Click_rgba(object sender, RoutedEventArgs e)
         {
-            var side = false;
-            while (int.Parse(speed.Text) >= 0)
+            var skBitmap = new SKBitmap(new SKImageInfo(1602, 768));
+            using (var canvas = new SKCanvas(skBitmap))
             {
-                
-                if (side)
-                {
-                    await LoadDCSSImage();
-                }
-                else
-                {
-                    await WriteAnotherImage();
-                }
-
-                side = !side;
-                await Task.Delay(int.Parse(speed.Text));
+                canvas.Clear(new SKColor(byte.Parse(colorr.Text), byte.Parse(colorg.Text), byte.Parse(colorb.Text),128));
             }
-            
+
+            output.Text = $"loaded, uint byte maxvalue = {colorr.Text} {colorg.Text}, {colorb.Text}";
+
+            var bitmapSource = new WriteableBitmap(1602, 768);
+            using (Stream stream = bitmapSource.PixelBuffer.AsStream())
+            {
+                //write to bitmap
+                await stream.WriteAsync(skBitmap.Bytes, 0, skBitmap.Bytes.Length).ConfigureAwait(false);
+            }
+            myImage.Source = bitmapSource;
+        }
+        private async void Button_Click_rgb(object sender, RoutedEventArgs e)
+        {
+            var skBitmap = new SKBitmap(new SKImageInfo(1602, 768));
+            using (var canvas = new SKCanvas(skBitmap))
+            {
+                canvas.Clear(new SKColor(byte.Parse(colorr.Text), byte.Parse(colorg.Text), byte.Parse(colorb.Text)));
+            }
+
+            output.Text = $"loaded, uint byte maxvalue = {colorr.Text} {colorg.Text}, {colorb.Text}";
+
+            var bitmapSource = new WriteableBitmap(1602, 768);
+            using (Stream stream = bitmapSource.PixelBuffer.AsStream())
+            {
+                //write to bitmap
+                await stream.WriteAsync(skBitmap.Bytes, 0, skBitmap.Bytes.Length).ConfigureAwait(false);
+            }
+            myImage.Source = bitmapSource;
+        }
+        private async void Button_Click_red(object sender, RoutedEventArgs e)
+        {
+            var skBitmap = new SKBitmap(new SKImageInfo(1602, 768));
+            using (var canvas = new SKCanvas(skBitmap))
+            {
+                canvas.Clear(SKColors.Red);
+            }
+
+            output.Text = $"loaded, skcolors.Red {SKColors.Red.Red}, {SKColors.Red.Green}, {SKColors.Red.Blue}";
+
+            var bitmapSource = new WriteableBitmap(1602, 768);
+            using (Stream stream = bitmapSource.PixelBuffer.AsStream())
+            {
+                //write to bitmap
+                await stream.WriteAsync(skBitmap.Bytes, 0, skBitmap.Bytes.Length).ConfigureAwait(false);
+            }
+            myImage.Source = bitmapSource;
+        }
+        private async void Button_Click_green(object sender, RoutedEventArgs e)
+        {
+            var skBitmap = new SKBitmap(new SKImageInfo(1602, 768));
+            using (var canvas = new SKCanvas(skBitmap))
+            {
+                canvas.Clear(SKColors.Green);
+            }
+
+            output.Text = $"loaded, skcolors.green {SKColors.Green.Red}, {SKColors.Green.Green}, {SKColors.Green.Blue}";
+
+            var bitmapSource = new WriteableBitmap(1602, 768);
+            using (Stream stream = bitmapSource.PixelBuffer.AsStream())
+            {
+                //write to bitmap
+                await stream.WriteAsync(skBitmap.Bytes, 0, skBitmap.Bytes.Length).ConfigureAwait(false);
+            }
+            myImage.Source = bitmapSource;
         }
 
 
-        private async Task ExtractExtraFileFolder(Stream stream)
-        {
-            try
-            {
-                var path = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), @"Extra.zip");
-                var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
-                Console.WriteLine(localFolder.Path);
-                var folder = await localFolder.CreateFolderAsync("Extra", CreationCollisionOption.OpenIfExists);
 
-                var zipInStream = new ZipInputStream(stream);
-                var entry = zipInStream.GetNextEntry();
-                while (entry != null && entry.CanDecompress)
-                {
-                    var outputFile = Path.Combine(folder.Path, entry.Name);
-
-                    var outputDirectory = Path.GetDirectoryName(outputFile);
-                    //Console.WriteLine(outputDirectory);
-                    var correctFolder = await folder.CreateFolderAsync(outputDirectory, CreationCollisionOption.OpenIfExists);
-
-
-                    if (entry.IsFile)
-                    {
-
-                        int size;
-                        byte[] buffer = new byte[zipInStream.Length];
-                        //Debug.WriteLine(buffer.Length);
-                        zipInStream.Read(buffer, 0, buffer.Length);
-                        File.WriteAllBytes(outputFile, buffer);
-                        //var bitmap = SKBitmap.Decode(buffer);
-                        ////Console.WriteLine(bitmap.ByteCount);
-                        //Console.WriteLine(bitmap.Height);
-                        //SKBitmap bitmap = SKBitmap.Decode(buffer);
-                        //Console.WriteLine(bitmap.ByteCount);
-                    }
-
-                    entry = zipInStream.GetNextEntry();
-                }
-                zipInStream.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-        private async Task<List<StorageFile>> GetFilesFromFolderAndSubfolders(string foldername)
-        {
-            var files = new List<StorageFile>();
-            try
-            {
-                
-                var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
-                var folder = await localFolder.GetFolderAsync(foldername);
-                Debug.WriteLine(folder.Path);
-                
-                files.AddRange((await folder.GetFilesAsync()).Where(file => file.Name.EndsWith("png",true,CultureInfo.InvariantCulture)));
-
-                var subFolders = await folder.GetFoldersAsync();
-                foreach (var subFolder in subFolders)
-                {
-                    files.AddRange((await subFolder.GetFilesAsync()).Where(file => file.Name.EndsWith("png", true, CultureInfo.InvariantCulture)));
-                }
-                Debug.WriteLine($"loaded {files.Count} files from {foldername}");
-                
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            return files;
-        }
-
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            await LoadDCSSImage();
-        }
         private async void Button_Click2(object sender, RoutedEventArgs e)
         {
             await WriteAnotherImage();
         }
-        private async void Button_Click3(object sender, RoutedEventArgs e)
-        {
-            await StartImageLoop();
-        }
+
     }
 }
